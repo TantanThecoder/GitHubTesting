765a35118f6cfc7654c1d5899d26ce8534e359c4
Thege <jonathan.thege@hotmail.com>
2023-01-23 16:17:31 +0100

    Adding a function that asks if the user likes apple or not.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Next shout!");
         System.out.println("I wanna make a more complex program");
         System.out.println("You are doing it wrong");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you like apples?");
+        String likesApples = sc.nextLine();
+        System.out.println(likesApples);
+
     }
 
 }
