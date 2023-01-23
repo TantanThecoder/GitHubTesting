39cbe5ebda39485274b0e025ac53cd50271e39e3
Thege <jonathan.thege@hotmail.com>
2023-01-23 16:09:01 +0100

    Make this interesting

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Next shout!");
+        System.out.println("I wanna make a more complex program");
     }
 
 }
