aaf9d3a1a5942e51baeeb3671ff6997070c45153
Thege <jonathan.thege@hotmail.com>
2023-01-23 16:08:24 +0100

    Added a new print

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Next shout!");
+        System.out.println("You are doing it wrong");
     }
 
 }
